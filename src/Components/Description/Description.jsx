7238402a05e01f8255b7a14cf9dea1233aba5c61
Milaya78 <ela_keyt@mail.ru>
2024-09-30 16:44:36 +0200

    text

--- src/Components/Description/Description.jsx
+++ src/Components/Description/Description.jsx
@@ -4,10 +4,7 @@ function Description() {
   return (
     <div className={css.container}>
       <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <p></p>
     </div>
   );
 }
